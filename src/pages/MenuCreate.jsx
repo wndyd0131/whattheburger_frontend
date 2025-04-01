55ef52db423cc93780e0add4b5a51b88c86156c9
wndyd <wndyd0131@naver.com>
2025-04-02 00:00:37 +0900

    add selectedOptionBlock to optionModal, sync optionGrid and selectedOptionBlock

--- src/pages/MenuCreate.jsx
+++ src/pages/MenuCreate.jsx
@@ -13,6 +13,7 @@ const MenuCreate = () => {
   const [briefInfo, setBriefInfo] = useState("");
   const [categories, setCategories] = useState([]);
   const [options, setOptions] = useState([]);
+  const [selectedOptions, setSelectedOptions] = useState([]);
   const [selectedCategory, setSelectedCategory] = useState("");
   const [customRuleName, setcustomRuleName] = useState("");
   const [customRuleType, setCustomRuleType] = useState("");
@@ -55,6 +56,24 @@ const MenuCreate = () => {
     setAddOptionButtonClicked(true);
   }
 
+  const handleClickOptionGrid = (optionIdx) => {
+    setSelectedOptions(prev => {
+      const exists = prev.find(option => option.id === optionIdx);
+      const newOption = {
+        id: optionIdx,
+        item: options[optionIdx]
+      }
+      if (exists) {
+        return prev.filter(option => option.id !== optionIdx);
+      }
+      else {
+        return (
+          [...prev, newOption]
+        );
+      }
+    });
+  }
+
   useEffect(() => {
     axios.get("http://localhost:8080/api/v1/category")
     .then(response => {
@@ -88,8 +107,9 @@ const MenuCreate = () => {
               <div className={styles.optionModalItemSection}>
                 <div className={styles.optionModalGridContainer}>
                   {options.map((option, optionIdx) => {
+                    const exists = selectedOptions.find(item => item.id === optionIdx);
                     return (
-                      <div key={optionIdx} className={styles.optionModalGrid}>
+                      <div key={optionIdx} className={`${styles.optionModalGrid} ${exists ? styles.checked : ""}`} onClick={() => handleClickOptionGrid(optionIdx)}>
                         <div className={styles.optionImageContainer}>
                         </div>
                         <div className={styles.optionDetailContainer}>
@@ -101,7 +121,22 @@ const MenuCreate = () => {
                   })}
                 </div>
               </div>
-            </div>            
+            </div>
+            <div className={styles.selectedOptionSection}>
+              {selectedOptions.map((option, optionIdx) => {
+                return (
+                  <div key={optionIdx} className={styles.selectedOptionBlock}>
+                    <div className={styles.optionImageContainer}>
+                    </div>
+                    <div className={styles.optionDetailContainer}>
+                      <p>{option.item.optionName}</p>
+                      <p>{option.item.optionCalories} Cal</p>
+                    </div>
+                  </div>
+                );
+
+              })}
+            </div>
             <div className={styles.optionModalFooter}>
             </div>
           </div>
