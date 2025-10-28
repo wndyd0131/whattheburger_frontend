eb4ef0b1ac3e30f8d6a6eb574dd0c92d84fcea2a
wndyd <wndyd0131@naver.com>
2025-10-28 09:24:24 +0900

    chore: disable strict mode

--- src/main.jsx
+++ src/main.jsx
@@ -1,5 +1,6 @@
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import { UserProvider } from './contexts/UserContext'
 
 createRoot(document.getElementById('root')).render(
   <UserProvider>
